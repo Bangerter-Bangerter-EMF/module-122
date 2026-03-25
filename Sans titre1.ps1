f05ebef7eff125ed526111ee001719eb2a9e3af9
bangerter-bangerter-emf <andy.bangerter@studentfr.ch>
2026-03-25 08:00:47 +0100

    gfd

---
+++ Sans titre1.ps1
@@ -0,0 +1,56 @@
+﻿$user = 1..5
+
+$user[0] = [PSCustomObject]@{
+    Nom         = "Dupont"
+    Prenom      = "Marie" 
+    Departement = "RH" 
+    Email       = "marie.dupont@entreprise.ch"
+    actif       = $true
+}
+
+$user[1] = [PSCustomObject]@{
+    Nom         = "Martin"
+    Prenom      = "Lucas" 
+    Departement = "Informatique" 
+    Email       = "lucas.martin@entreprise.ch"
+    actif       = $true
+}
+
+
+
+$user[2] = [PSCustomObject]@{
+    Nom         = "bernard"
+    Prenom      = "Sophie" 
+    Departement = "Marketing" 
+    Email       = "sophie.Bernard@entreprise.ch"
+    actif       = $true
+}
+
+
+$user[3] = [PSCustomObject]@{
+    Nom         = "Petit"
+    Prenom      = "Julien" 
+    Departement = "Finance" 
+    Email       = "julien.Petit@entreprise.ch"
+    actif       = $false
+}
+
+
+$user[4] = [PSCustomObject]@{
+    Nom         = "Robert"
+    Prenom      = "Claire" 
+    Departement = "Informatique" 
+    Email       = "claire.robert@entreprise.ch"
+    actif       = $false
+}
+
+
+
+
+for ($i = 0; $i -lt $user.Count; $i++) {
+    if ($user[$i].Departement -like "Informatique" ) {
+        $user[$i]
+    }
+}
+
+$users | Export-Csv -Path "C:\Users\BangerterA\OneDrive - EDUETATFR\dossier EMF Andy Bangerter\A-EMF informatique\module 122\exercices\People.csv" -Delimiter ';' -NoTypeInformation
---
+++ Sans titre2.ps1
@@ -0,0 +1,37 @@
+﻿$users=1..5
+
+$users[0]=[PSCustomObject]@{
+        Nom = "dupont";
+        Prenom = "Marie";
+        Departement= "RH";
+        Email = "marie.dupont@entreprise.ch"
+    }
+  $users[1]=[PSCustomObject]@{
+        Nom = "Martin";
+        Prenom = "Lucas";
+        Departement= "Informatique";
+        Email = "lucas.martin@entreprise.ch"}
+  $users[2]=[PSCustomObject]@{
+        Nom = "bernard";
+        Prenom = "Sophie";
+        Departement= "Marketing";
+        Email = "sophie.bernard@entreprise.ch"}
+  $users[3]   =[PSCustomObject]@{
+        Nom = "petit";
+        Prenom = "julien";
+        Departement= "Finance";
+        Email = "julien.petit@entreprise.ch"}
+   $users[4]=[PSCustomObject]@{
+        Nom = "robert";
+        Prenom = "claire";
+        Departement= "Informatique";
+        Email = "claire.robertn@entreprise.ch"}
+  
+
+
+for ($i = 0; $i -lt $user.Count; $i++) {
+    if ($user[$i].Departement -like "Informatique" ) {
+        $user[$i]
+    }
+}
+
